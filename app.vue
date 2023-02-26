85205d5593f5f5bff3d3d6734cdb26fba4e8997a
poxe <poxe@MacBook-Pro-de-poxe.local>
2023-02-26 20:18:36 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <Head>
+      <Title>OrdinalBoard - The immortal map</Title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    </Head>
+    <header>
+      <BigButton />
+    </header>
+    <main>
+      <p>Hey Legend ! We'll be there in the next few minutes...</p>
+      <p>
+        <a href="https://discord.gg/3d634w89Vk" target="_blank">Join the discord</a>
+      </p>
+    </main>
+  </div>
+</template>
+
+<script>
+import BigButton from '@/components/BigButton/index.vue';
+
+export default {
+  components: {
+    BigButton,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  background-color: $blackColor;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow-y: auto;
+
+  header {
+    max-width: 480px;
+    margin: 0 auto;
+    margin-top: 10vh;
+
+    .big-button {
+      font-size: 4rem;
+      height: 82px;
+      line-height: 82px;
+
+      .ti-cursor {
+        font-size: 4rem;
+      }
+    }
+  }
+
+  main {
+    max-width: 480px;
+    margin: 42px auto;
+
+    p {
+      font-size: 22px;
+      color: gainsboro;
+    }
+  }
+}
+</style>
---
+++ components/BigButton/index.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="big-button">
+        <span ref="bigbuttontext"></span>
+    </div>
+</template>
+
+<script>
+import TypeIt from "typeit";
+
+export default {
+    mounted() {
+        const text = this.$refs['bigbuttontext']
+
+        const typer = new TypeIt(text, {
+            afterComplete: () => {
+                // some animations to make here
+            }
+        })
+
+        typer
+            .type('Be Eternal')
+            .pause(750)
+            .delete(7)
+            .type('Immortal')
+            .pause(750)
+            .delete(8)
+            .type('Ordinal')
+            .pause(1250)
+            .delete(11)
+            .type("Ordinal Board")
+
+        typer.go()
+    },
+}
+</script>
+
+<style lang="scss">
+.big-button {
+    display: inline-block;
+    box-sizing: border-box;
+    border: none;
+    outline: none;
+    position: relative;
+    z-index: 0;
+    border-radius: 2px;
+    padding: 0 10px;
+    color: $primaryColor;
+    text-transform: uppercase;
+    letter-spacing: 0.5px;
+    font-size: 22px;
+    width: 100%;
+    height: 42px;
+    line-height: 42px;
+    text-align: center;
+}
+
+.big-button::before {
+}
+
+.big-button:hover {
+    text-decoration: underline;
+    color: $primaryColor !important;
+}
+
+.big-button:hover:before {
+    background: $primaryColor !important;
+}
+
+.big-button::after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: calc(100% + 20px);
+    height: calc(100% + 20px);
+    background: #111;
+    left: -10px;
+    top: -10px;
+    border-radius: 2px;
+}
+
+@keyframes glowing {
+    0% {
+        background-position: 0 0;
+    }
+
+    50% {
+        background-position: 400% 0;
+    }
+
+    100% {
+        background-position: 0 0;
+    }
+}
+</style>
